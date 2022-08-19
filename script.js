36449897b0537e04f4842256f0f14b1afc157f30
AlxTecnopata <cortez.alejandro35@gmail.com>
2022-08-19 01:40:02 -0500

    Terminado lml

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-const textInput = document.querySelector("#box")
+const textinput = document.querySelector(".intext")
+const textoutput = document.querySelector(".outtext")
 
-function cript() {
-  var textenc = textInput.value
-  var textproc = textenc.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-  textproc = textproc.toLowerCase()
-    .replaceAll("e", "enter")
-    .replaceAll("i", "imes")
-    .replaceAll("a", "ai")
-    .replaceAll("o", "ober")
-    .replaceAll("u", "ufat");
+// ######## BOTÓN DE ENCRIPTAR ########
 
-    if (textenc == "") {
-    console.log("Ingrese el texto aquí");
-    } else {
-    document.querySelector("#screen").innerHTML = textproc;
+function btnEncript() {
+  const encriptado = encript(textinput.value)
+  textoutput.value = encriptado;
+  textoutput.style.backgroundImage = "none"
+  textinput.value = "";
 
-  };
 }
 
-// function desencriptar() {
-//   var textModif = textInput.value
-//   var newText = textModif.toLowerCase()
-//     .replaceAll("enter", "e")
-//     .replaceAll("imes", "i")
-//     .replaceAll("ai", "a")
-//     .replaceAll("ober", "o")
-//     .replaceAll("ufat", "u");
-//
-//   if (textModif === "") {
-//     console.log("escribe algo")
-//   } else {
-//     document.querySelector("#textDesencripted").innerHTML = newText;
-//     console.log(newText);
-//   }
-// }
-//
-// function copy(){
-//   var textArea = document.getElementById("textDesencripted");
-//   var copyText = textArea.innerHTML;
-//   navigator.clipboard.writeText(copyText);
-//
-//   let notify = document.getElementById("popCopy");
-//   notify.innerHTML = "Copiado!";
-//   notify.style.display = "block";
-//   setTimeout(function() {
-//     notify.style.display = "none";
-//   }, 3000);
-// }
+function encript(encripted){
+  let matrixCode = [ ["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+  encripted = encripted.toLowerCase();
+  for (let i = 0; i < matrixCode.length; i++){
+    if(encripted.includes(matrixCode[i][0])) {
+      encripted = encripted.replaceAll(matrixCode[i][0],matrixCode[i][1])
+      }
+  }
+return encripted;
+}
+// #######################################
+
+// ######## BOTÓN DE DESENCRIPTAR ########
+function btnDesencript() {
+  const encriptado = decript(textinput.value)
+  textoutput.value = encriptado;
+  textoutput.style.backgroundImage = "none"
+  textinput.value = "";
+
+}
+
+function decript(decripted){
+  let matrixCode = [ ["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+  decripted = decripted.toLowerCase();
+  for (let i = 0; i < matrixCode.length; i++){
+    if(decripted.includes(matrixCode[i][1])) {
+      decripted = decripted.replaceAll(matrixCode[i][1],matrixCode[i][0])
+      }
+  }
+return decripted;
+}
+// #######################################
+
+// ######## BOTÓN DE copiar ########
+function btnCopy() {
+  textinput.select()
+  navigator.clipboard.writeText(textoutput.value)
+  textoutput.value = ""
+  textoutput.style.backgroundImage = "url('img/Muñeco.png')";
+}
+
+function btnErase() {
+  textinput.select()
+  textoutput.value = ""
+  textoutput.style.backgroundImage = "url('img/Muñeco.png')";
+}
